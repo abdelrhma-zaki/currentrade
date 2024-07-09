00c6521864c9f0fe50f88a1a4571bb89aa988057
Abdelrhman Zaki <75377023+abdelrhma-zaki@users.noreply.github.com>
2024-07-09 06:39:40 +0300

    Update queries.control.js

--- back/queries.control.js
+++ back/queries.control.js
@@ -1,5 +1,123 @@
-const connection = require("./connection.js");
-console.log("api work");
+// const connection = require("./connection.js");
+// console.log("api work");
+// const jwt = require("jsonwebtoken");
+// const express = require("express");
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// const secretKey = process.env.SECRETKEY;
+
+// const router = express.Router();
+
+// router.get(`/account/${process.env.api1}`, (req, res) => {
+//   connection.query("SELECT * FROM account", (error, results, fields) => {
+//     if (error) throw error;
+//     res.json({ message: "Done", results: results });
+//   });
+// });
+
+// router.post("/account/login", (req, res) => {
+//   const { email, password } = req.body;
+
+//   connection.query(
+//     "SELECT * FROM account WHERE email= ? ",
+//     [email],
+//     (error, results, fields) => {
+//       if (error) throw error;
+//       if (results.length == 0) {
+//         return res
+//           .status(401)
+//           .send({ message: "this email is not in the database" });
+//       } else {
+//         if (password === results[0].password) {
+//           const token = jwt.sign({ email, password }, secretKey, {
+//             expiresIn: "1h",
+//           });
+
+//           res.cookie("token", token, { httpOnly: true });
+//           return res.json({ email });
+//         } else {
+//           return res.status(401).json({ message: "incorrect password" });
+//         }
+//       }
+//     }
+//   );
+
+// });
+
+// router.post("/account/register", (req, res) => {
+//   const { email, password } = req.body;
+// console.log("email :",email);
+// console.log("password :",password);
+//   connection.query(
+//     "SELECT * FROM account WHERE email= ? ",
+//     [email],
+//     (error, results, fields) => {
+//       if (error) throw error;
+//       if (results.length == 0) {
+//         const token = jwt.sign({ email, password }, secretKey, {
+//           expiresIn: "1h",
+//         });
+//         connection.query(
+//           "insert into account (email , password , balance) values ( ? , ? , 0 )",
+//           [email, password]
+//         );
+//         res.cookie("token", token, { httpOnly: true });
+
+//         return res.json({ email });
+//       } else {
+//         return res
+//           .status(401)
+//           .json({ message: "this email is already in the database" });
+//       }
+//     }
+//   );
+// });
+
+// router.post("/token", (req, res) => {
+//   const token = req.cookies.token;
+//   if (!token) {
+//     return res.status(401).json({ message: "No token provided" });
+//   }
+  
+//   const decoded = jwt.verify(token, secretKey);
+//   connection.query(
+//     "select password , balance from account where email = ?",
+//     [decoded.email],
+//     (error, results, fields) => {
+//       if (results.length == 0) {
+//         return res
+//           .status(401)
+//           .send({ message: "this email is not in the database" });
+//       } else {
+//         if (decoded.password === results[0].password) {
+//           console.log('Tamam');
+//           return res.json({ message: "Done", balance: results[0].balance });
+//         } else {
+//           console.log('M4 Tamam');
+//           return res.status(401).json({ message: "Invalid token" });
+//         }
+//       }
+//     }
+//   );
+// });
+
+// router.post("/account/update-balance", (req, res) => {
+//       const balance = Number(req.body.amount) || 0;
+//       console.log("balance",balance)
+
+//       connection.query(
+//         `update account set balance = balance + ? where email = ?`,
+//         [ balance, req.body.email],
+//         (error, results, fields) => {
+//           if (error) throw error;
+//           res.json({ message: "Done" });
+//         }
+//       );
+// });
+
+// module.exports = router;
+const Account = require("./accountModel");
 const jwt = require("jsonwebtoken");
 const express = require("express");
 const dotenv = require("dotenv");
@@ -9,117 +127,94 @@ const secretKey = process.env.SECRETKEY;
 
 const router = express.Router();
 
-router.get(`/account/${process.env.api1}`, (req, res) => {
-  connection.query("SELECT * FROM account", (error, results, fields) => {
-    if (error) throw error;
-    res.json({ message: "Done", results: results });
-  });
+router.get(`/account/${process.env.api1}`, async (req, res) => {
+  try {
+    const accounts = await Account.find();
+    res.json({ message: "Done", results: accounts });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-router.post("/account/login", (req, res) => {
+router.post("/account/login", async (req, res) => {
   const { email, password } = req.body;
 
-  // connection.query(
-  //   "SELECT * FROM account WHERE email= ? ",
-  //   [email],
-  //   (error, results, fields) => {
-  //     if (error) throw error;
-  //     if (true) {
-  //       return res
-  //         .status(401)
-  //         .send({ message: "this email is not in the database" });
-  //     } else {
-  //       if (password === results[0].password) {
-  //         const token = jwt.sign({ email, password }, secretKey, {
-  //           expiresIn: "1h",
-  //         });
-
-  //         res.cookie("token", token, { httpOnly: true });
-  //         return res.json({ email });
-  //       } else {
-  //         return res.status(401).json({ message: "incorrect password" });
-  //       }
-  //     }
-  //   }
-  // );
-  connection.query(
-          "insert into account (email , password , balance) values ( ? , ? , 0 )",
-          [email, password]
-        );
-        res.cookie("token", token, { httpOnly: true });
-  connection.end();
-        return res.json({ email });
-});
-
-router.post("/account/register", (req, res) => {
-  const { email, password } = req.body;
-console.log("email :",email);
-console.log("password :",password);
-  connection.query(
-    "SELECT * FROM account WHERE email= ? ",
-    [email],
-    (error, results, fields) => {
-      if (error) throw error;
-      if (true) {
-        const token = jwt.sign({ email, password }, secretKey, {
-          expiresIn: "1h",
-        });
-        connection.query(
-          "insert into account (email , password , balance) values ( ? , ? , 0 )",
-          [email, password]
-        );
-        res.cookie("token", token, { httpOnly: true });
-
-        return res.json({ email });
-      } else {
-        return res
-          .status(401)
-          .json({ message: "this email is already in the database" });
-      }
+  try {
+    const account = await Account.findOne({ email });
+    if (!account) {
+      return res.status(401).json({ message: "this email is not in the database" });
     }
-  );
+
+    if (password === account.password) {
+      const token = jwt.sign({ email, password }, secretKey, { expiresIn: "1h" });
+      res.cookie("token", token, { httpOnly: true });
+      return res.json({ email });
+    } else {
+      return res.status(401).json({ message: "incorrect password" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-router.post("/token", (req, res) => {
+router.post("/account/register", async (req, res) => {
+  const { email, password } = req.body;
+  console.log("email :", email);
+  console.log("password :", password);
+
+  try {
+    const existingAccount = await Account.findOne({ email });
+    if (existingAccount) {
+      return res.status(401).json({ message: "this email is already in the database" });
+    }
+
+    const account = new Account({ email, password });
+    await account.save();
+
+    const token = jwt.sign({ email, password }, secretKey, { expiresIn: "1h" });
+    res.cookie("token", token, { httpOnly: true });
+    return res.json({ email });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post("/token", async (req, res) => {
   const token = req.cookies.token;
   if (!token) {
     return res.status(401).json({ message: "No token provided" });
   }
-  
-  const decoded = jwt.verify(token, secretKey);
-  connection.query(
-    "select password , balance from account where email = ?",
-    [decoded.email],
-    (error, results, fields) => {
-      if (true) {
-        return res
-          .status(401)
-          .send({ message: "this email is not in the database" });
-      } else {
-        if (decoded.password === results[0].password) {
-          console.log('Tamam');
-          return res.json({ message: "Done", balance: results[0].balance });
-        } else {
-          console.log('M4 Tamam');
-          return res.status(401).json({ message: "Invalid token" });
-        }
-      }
+
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    const account = await Account.findOne({ email: decoded.email });
+
+    if (!account) {
+      return res.status(401).json({ message: "this email is not in the database" });
     }
-  );
+
+    if (decoded.password === account.password) {
+      console.log('Tamam');
+      return res.json({ message: "Done", balance: account.balance });
+    } else {
+      console.log('M4 Tamam');
+      return res.status(401).json({ message: "Invalid token" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-router.post("/account/update-balance", (req, res) => {
-      const balance = Number(req.body.amount) || 0;
-      console.log("balance",balance)
+router.post("/account/update-balance", async (req, res) => {
+  const balance = Number(req.body.amount) || 0;
+  console.log("balance", balance);
 
-      connection.query(
-        `update account set balance = balance + ? where email = ?`,
-        [ balance, req.body.email],
-        (error, results, fields) => {
-          if (error) throw error;
-          res.json({ message: "Done" });
-        }
-      );
+  try {
+    await Account.updateOne({ email: req.body.email }, { $inc: { balance } });
+    res.json({ message: "Done" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = router;
