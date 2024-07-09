1bac7b65f29388218e0dc171c1ff3e22e35dac34
Abdelrhman Zaki <75377023+abdelrhma-zaki@users.noreply.github.com>
2024-07-09 04:06:30 +0300

    Update connection.js

--- back/connection.js
+++ back/connection.js
@@ -1,28 +1,54 @@
 
-const mysql = require('mysql');
+// const mysql = require('mysql');
 const dotenv = require('dotenv');
 dotenv.config();
 
 // إنشاء اتصال بقاعدة البيانات
-const connection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER, // اسم المستخدم لقاعدة البيانات
-  password: process.env.PASSWORD, // كلمة المرور لقاعدة البيانات
-  database: process.env.DATABASE // اسم قاعدة البيانات
-});
+// const connection = mysql.createConnection({
+//   host: process.env.HOST,
+//   user: process.env.USER, // اسم المستخدم لقاعدة البيانات
+//   password: process.env.PASSWORD, // كلمة المرور لقاعدة البيانات
+//   database: process.env.DATABASE // اسم قاعدة البيانات
+// });
 
-// currentrader
-// err.stack
-// فتح الاتصال
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:' );
-    return;
-  }
-  console.log('Connected to the database as id ' + connection.threadId);
-});
+// // currentrader
+// // err.stack
+// // فتح الاتصال
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to the database:' );
+//     return;
+//   }
+//   console.log('Connected to the database as id ' + connection.threadId);
+// });
 
-// إجراء استعلام كمثال
-module.exports = connection;
+// // إجراء استعلام كمثال
+// module.exports = connection;
 // إغلاق الاتصال
 // connection.end();
+
+const mysql = require('mysql2');
+
+// إعداد الاتصال بقاعدة البيانات باستخدام URL
+const url = "mysql://root:vjZbdRjuTcjjDnJIFilAOHpGEoEmxlxT@monorail.proxy.rlwy.net:34782/railway";
+
+// إنشاء اتصال بقاعدة البيانات
+const connection = mysql.createConnection(url);
+
+// اتصال بقاعدة البيانات
+connection.connect((err) => {
+  if (err) {
+    console.error('Error:', err.stack);
+    return;
+  }
+  console.log('db connected');
+});
+
+// تنفيذ استعلام بسيط
+// connection.query('SELECT * FROM table_name', (err, results, fields) => {
+//   if (err) {
+//     console.error('حدث خطأ أثناء تنفيذ الاستعلام:', err.stack);
+//     return;
+//   }
+//   console.log('النتائج:', results);
+// });
